7478232c27aeab433d647e0d502923bc6b04d46c
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2024-09-03 22:31:48 +1200

    Update luhn.py

--- luhn.py
+++ luhn.py
@@ -16,13 +16,18 @@ def verify_card_number(card_number):
             number = (number // 10) + (number % 10)
         sum_of_even_digits += number
     total = sum_of_odd_digits + sum_of_even_digits
-    print(total)
     return total % 10 == 0
 
-def main():
-    card_number = '4111-1111-4555-1141'
+def luhn():
+    card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
+
+    if verify_card_number(translated_card_number):
+        print('VALID!')
+    else:
+        print('INVALID!')
+luhn()
 
     if verify_card_number(translated_card_number):
         print('VALID!')
